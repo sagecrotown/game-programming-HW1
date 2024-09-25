3fe0c0453897dd2c91a8834f233fb6df26ae8861
Sage <sage@crotown.com>
2024-09-25 14:38:41 -0400

    Initial Commit

---
+++ homework_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework_1
+//
+//  Created by Sage Cronen-Townsend on 9/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
